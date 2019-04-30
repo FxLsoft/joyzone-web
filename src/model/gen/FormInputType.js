5112754083dbc6020cd7419289ab376b67fdba3f
xiaoliang.feng <xiaoliang.feng@renren-inc.com>
2019-04-30 18:56:52 +0800

    项目完善

---
+++ src/model/gen/FormInputType.js
@@ -0,0 +1,11 @@
+/**
+ * 表单输入类型
+ */
+export default [
+    {
+        key: '单行文本',
+        value: 'input'
+    }, {
+        key: ''
+    }
+]
---
+++ src/model/gen/PhysicsType.js
@@ -0,0 +1,28 @@
+/**
+ * 物理类型
+ */
+
+ export default [
+    {
+        key: 'varchar(64)',
+        value: 'varchar(64)'
+    }, {
+        key: 'nvarchar(64)',
+        value: 'nvarchar(64)'
+    }, {
+        key: 'integer',
+        value: 'integer'
+    }, {
+        key: 'double',
+        value: 'double'
+    }, {
+        key: 'datetime',
+        value: 'datetime'
+    }, {
+        key: 'longblob',
+        value: 'longblob'
+    }, {
+        key: 'longtext',
+        value: 'longtext'
+    }
+ ];
