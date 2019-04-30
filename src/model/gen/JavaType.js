edfdbfd965878af3afad710e90ce1db0dc63ffca
xiaoliang.feng <xiaoliang.feng@renren-inc.com>
2019-04-30 17:37:55 +0800

    项目完善

---
+++ src/model/gen/JavaType.js
@@ -0,0 +1,22 @@
+/**
+ * Java 类型
+ */
+
+export default [
+    {
+        key: 'String',
+        value: 'String'
+    }, {
+        key: 'Long',
+        value: 'Long'
+    }, {
+        key: 'Integer',
+        value: 'Integer'
+    }, {
+        key: 'Double',
+        value: 'Double'
+    }, {
+        key: 'Date',
+        value: 'java.util.Date'
+    }
+]
---
+++ src/model/gen/QueryMatchType.js
@@ -0,0 +1,37 @@
+/**
+ * 查询匹配方式
+ */
+
+ export default [
+    {
+        key: '=',
+        value: '='
+    }, {
+        key: '!=',
+        value: '!='
+    }, {
+        key: '>',
+        value: '>'
+    }, {
+        key: '>=',
+        value: '>='
+    }, {
+        key: '<',
+        value: '<'
+    }, {
+        key: '<=',
+        value: '<='
+    }, {
+        key: 'Between',
+        value: 'between'
+    }, {
+        key: 'Like',
+        value: 'like'
+    }, {
+        key: 'Left Like',
+        value: 'left_like'
+    }, {
+        key: 'Right Like',
+        value: 'right_like'
+    }
+]
